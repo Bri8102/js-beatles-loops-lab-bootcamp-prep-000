2a890d06f77d0fff0e3b97c40b2d64c22844b00b
Briana Oniru <damioniru16@gmail.com>
2020-07-07 01:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,15 @@ function theBeatlesPlay(musicians,instruments) {
 }
 
 function johnLennonFacts(array) {
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+let i = 0
+while (i < facts.length) {
+  console.log(`${facts} !!!`) 
+  i++;
   
+}
 }
